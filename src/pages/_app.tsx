67d96924d4337ce0a93ee50770b3de21e779dfb3
Porawet Ku <gporawet.ku@gmail.com>
2024-03-11 17:37:51 +0700

    chore: add theme in project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+// import "primereact/resources/themes/lara-light-cyan/theme.css";
+import Tailwind from "primereact/passthrough/tailwind";
+
+import { PrimeReactProvider } from "primereact/api";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const value = { unstyled: true, pt: Tailwind };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PrimeReactProvider value={value}>
+        <Component {...pageProps} />
+      </PrimeReactProvider>
+    </QueryClientProvider>
+  );
 }
